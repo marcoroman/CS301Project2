f88f7db2dc5cfe36d03b52ad6cad5971039e611d
Marco Roman <marcoroman77@yahoo.com>
2017-11-21 00:40:29 -0800

    True error implemented.

--- src/RootLocator.java
+++ src/RootLocator.java
@@ -11,53 +11,57 @@ public class RootLocator{
 
         writer = new PrintWriter(file);
 
+        double trueRoot1 = 0.36509824, trueRoot2 = 1.9217409, trueRoot3 = 3.5631608;
+
         /***************Finding the roots of the first function***************/
 
         writer.println("Roots of Function 1\n");
 
         writer.println("Bisection Method");
-        Bisection(1, 0, 1);
-        Bisection(1, 1, 3);
-        Bisection(1, 3, 4);
+        Bisection(1, 0, 1, trueRoot1);
+        Bisection(1, 1, 3, trueRoot2);
+        Bisection(1, 3, 4, trueRoot3);
 
         writer.println("Newton-Raphson Method");
-        Newton_Raphson(1, 0);
-        Newton_Raphson(1, 2);
-        Newton_Raphson(1, 3);
+        Newton_Raphson(1, 0, trueRoot1);
+        Newton_Raphson(1, 2, trueRoot2);
+        Newton_Raphson(1, 3, trueRoot3);
 
         writer.println("False Position Method");
-        FalsePosition(1, 0, 1);
-        FalsePosition(1, 1, 3);
-        FalsePosition(1, 3, 4);
+        FalsePosition(1, 0, 1, trueRoot1);
+        FalsePosition(1, 1, 3, trueRoot2);
+        FalsePosition(1, 3, 4, trueRoot3);
 
         writer.println("Secant Method");
-        Secant(1, 0, 1);
-        Secant(1, 1, 3);
-        Secant(1, 3, 4);
+        Secant(1, 0, 1, trueRoot1);
+        Secant(1, 1, 3, trueRoot2);
+        Secant(1, 3, 4, trueRoot3);
 
         writer.println("Modified Secant Method");
-        ModifiedSecant(1, 1, .01);
-        ModifiedSecant(1, 2, .01);
-        ModifiedSecant(1, 3, .01);
+        ModifiedSecant(1, 1, .01, trueRoot1);
+        ModifiedSecant(1, 2, .01, trueRoot2);
+        ModifiedSecant(1, 3, .01, trueRoot3);
 
         /***************Finding the roots of the second function***************/
 
-        writer.println("\n\nRoots of Function 2\n");
+        double trueRoot = 126.63244;
+
+        writer.println("\n\nRoot of Function 2\n");
 
         writer.println("Bisection Method");
-        Bisection(2, 120, 130);
+        Bisection(2, 120, 130, trueRoot);
 
         writer.println("Newton-Raphson Method");
-        Newton_Raphson(2, 120);
+        Newton_Raphson(2, 120, trueRoot);
 
         writer.println("False Position Method");
-        FalsePosition(2, 120, 130);
+        FalsePosition(2, 120, 130, trueRoot);
 
         writer.println("Secant Method");
-        Secant(2, 120, 130);
+        Secant(2, 120, 130, trueRoot);
 
         writer.println("Modified Secant Method");
-        ModifiedSecant(2, 120, .01);
+        ModifiedSecant(2, 120, .01, trueRoot);
 
         writer.close();
 
@@ -65,24 +69,26 @@ public class RootLocator{
     }
 
     //Arguments: (function #, guess A, guess B)
-    public static void Bisection(int fx, double doubleA, double doubleB){
-        double doubleC = 0, evalA = 0, evalB = 0, evalC = 0, doubleApproximateError = 1, prev = doubleC;
+    public static void Bisection(int fx, double doubleA, double doubleB, double root){
+        double doubleC = 0, evalA = 0, evalB = 0, evalC = 0, prev = doubleC;
+        double approximateError = 1, trueError = 1;
         int iteration = 0;
 
-        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
-                "n", "a", "b", "c", "f(a)", "f(b)", "f(c)", "Ea");
+        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+                "n", "a", "b", "c", "f(a)", "f(b)", "f(c)", "Ea", "Et");
 
-        while(doubleApproximateError >= .01){
+        while(approximateError >= .01){
             doubleC = (doubleA + doubleB) / 2.0;
 
-            doubleApproximateError = Math.abs((doubleC - prev) / doubleC);
+            approximateError = Math.abs((doubleC - prev) / doubleC);
+            trueError = Math.abs((root - doubleC) / root);
 
             evalA = evaluate(fx, false, doubleA);
             evalB = evaluate(fx, false, doubleB);
             evalC = evaluate(fx, false, doubleC);
 
-            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
-                    iteration++, doubleA, doubleB, doubleC, evalA, evalB, evalC, doubleApproximateError);
+            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
+                    iteration++, doubleA, doubleB, doubleC, evalA, evalB, evalC, approximateError, trueError);
 
             if((evalA >= 0 && evalC >= 0) || (evalA < 0 && evalC < 0)){
                 doubleA = doubleC;
@@ -96,23 +102,24 @@ public class RootLocator{
     }
 
     //Arguments: (function #, initial guess for x)
-    public static void Newton_Raphson(int fx, double start){
-        double doubleVariableX = start, doubleVariableNext = 0, doubleApproximateError = 1;
+    public static void Newton_Raphson(int fx, double start, double root){
+        double doubleVariableX = start, doubleVariableNext = 0, approximateError = 1, trueError = 1;
         int iteration = 0;
 
-        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s\n",
-                "n", "x", "x + 1", "f(x)", "d[f(x)]", "Ea");
+        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+                "n", "x", "x + 1", "f(x)", "d[f(x)]", "Ea", "Et");
 
-        while(doubleApproximateError >= .01){
+        while(approximateError >= .01){
             double evalFx = evaluate(fx, false, doubleVariableX);
             double eval_dFx = evaluate(fx, true, doubleVariableX);
 
             doubleVariableNext = doubleVariableX - (evalFx / eval_dFx);
 
-            doubleApproximateError = Math.abs((doubleVariableNext - doubleVariableX) / doubleVariableNext);
+            approximateError = Math.abs((doubleVariableNext - doubleVariableX) / doubleVariableNext);
+            trueError = Math.abs((root - doubleVariableNext) / root);
 
-            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f\n",
-                    iteration++, doubleVariableX, doubleVariableNext, evalFx, eval_dFx, doubleApproximateError);
+            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f\n",
+                    iteration++, doubleVariableX, doubleVariableNext, evalFx, eval_dFx, approximateError, trueError);
 
             doubleVariableX = doubleVariableNext;
         }
@@ -121,24 +128,26 @@ public class RootLocator{
     }
 
     //Arguments (function #, guess 1, guess 2)
-    public static void FalsePosition(int fx, double doubleA, double doubleB){
-        double doubleC = 0, evalA = 0, evalB = 0, evalC = 0, doubleApproximateError = 1, prev = doubleC;
+    public static void FalsePosition(int fx, double doubleA, double doubleB, double root){
+        double doubleC = 0, evalA = 0, evalB = 0, evalC = 0, prev = doubleC;
+        double approximateError = 1, trueError = 1;
         int iteration = 0;
 
-        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
-                "n", "a", "b", "c", "f(a)", "f(b)", "f(c)", "Ea");
+        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+                "n", "a", "b", "c", "f(a)", "f(b)", "f(c)", "Ea", "Et");
 
-        while(doubleApproximateError >= .01){
+        while(approximateError >= .01){
             evalA = evaluate(fx, false, doubleA);
             evalB = evaluate(fx, false, doubleB);
 
             doubleC = ((doubleA * evalB) - (doubleB * evalA)) / (evalB - evalA);
             evalC = evaluate(fx, false, doubleC);
 
-            doubleApproximateError = Math.abs((doubleC - prev) / doubleC);
+            approximateError = Math.abs((doubleC - prev) / doubleC);
+            trueError = Math.abs((root - doubleC) / root);
 
-            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
-                    iteration++, doubleA, doubleB, doubleC, evalA, evalB, evalC, doubleApproximateError);
+            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
+                    iteration++, doubleA, doubleB, doubleC, evalA, evalB, evalC, approximateError, trueError);
 
             if((evalA >= 0 && evalC >= 0) || (evalA < 0 && evalC < 0)){
                 doubleA = doubleC;
@@ -152,24 +161,25 @@ public class RootLocator{
     }
 
     //Arguments: (function #, x - 1, x)
-    public static void Secant(int fx, double x0, double x1){
-        double x2 = 0, evalX0 = 0, evalX1 = 0, evalX2 = 0, doubleApproximateError = 1;
+    public static void Secant(int fx, double x0, double x1, double root){
+        double x2 = 0, evalX0 = 0, evalX1 = 0, evalX2 = 0, approximateError = 1, trueError = 1;
         int iteration = 0;
 
-        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
-                "n", "x-1", "x", "x+1", "f(x-1)", "f(x)", "f(x+1)", "Ea");
+        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+                "n", "x-1", "x", "x+1", "f(x-1)", "f(x)", "f(x+1)", "Ea", "Et");
 
-        while(doubleApproximateError >= .01){
+        while(approximateError >= .01){
             evalX0 = evaluate(fx, false, x0);
             evalX1 = evaluate(fx, false, x1);
 
             x2 = x1 - (evalX1 * ((x1 - x0) / (evalX1 - evalX0)));
             evalX2 = evaluate(fx, false, x2);
 
-            doubleApproximateError = Math.abs((x1 - x0) / x1);
+            approximateError = Math.abs((x1 - x0) / x1);
+            trueError = Math.abs((root - x2) / root);
 
-            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
-                    iteration++, x0, x1, x2, evalX0, evalX1, evalX2, doubleApproximateError);
+            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
+                    iteration++, x0, x1, x2, evalX0, evalX1, evalX2, approximateError, trueError);
 
             x0 = x1;
             x1 = x2;
@@ -179,14 +189,14 @@ public class RootLocator{
     }
 
     //Arguments: (function #, initial guess for x, delta value)
-    public static void ModifiedSecant(int fx, double x, double delta){
-        double x1 = 0, deltaX = 0, evalX = 0, evalDx = 0, doubleApproximateError = 1;
+    public static void ModifiedSecant(int fx, double x, double delta, double root){
+        double x1 = 0, deltaX = 0, evalX = 0, evalDx = 0, approximateError = 1, trueError = 1;
         int iteration = 0;
 
-        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s\n",
-                "n", "x", "Dx", "x+1", "f(x)", "f(x + Dx)", "Ea");
+        writer.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
+                "n", "x", "Dx", "x+1", "f(x)", "f(x + Dx)", "Ea", "Et");
 
-        while(doubleApproximateError >= .01){
+        while(approximateError >= .01){
             deltaX = x * delta;
 
             evalX = evaluate(fx,false, x);
@@ -194,10 +204,11 @@ public class RootLocator{
 
             x1 = x - (evalX * (deltaX / (evalDx - evalX)));
 
-            doubleApproximateError = Math.abs((x1 - x) / x1);
+            approximateError = Math.abs((x1 - x) / x1);
+            trueError = Math.abs((root - x1) / root);
 
-            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f\n",
-                    iteration++, x, deltaX, x1, evalX, evalDx, doubleApproximateError);
+            writer.printf("%-5d %-10f %-10f %-10f %-10f %-10f %-10f %-10f\n",
+                    iteration++, x, deltaX, x1, evalX, evalDx, approximateError, trueError);
 
             x = x1;
         }
